f3f7e2d5d0ebf6b7946080e0050ecedfc2af4b8b
John Paxton <johnpaxton@gmail.com>
2022-06-15 19:02:49 +0000

    Improved solution

--- labs/lab-02/solution/index.js
+++ labs/lab-02/solution/index.js
@@ -1,17 +1,32 @@
 import { people } from './data.js';
 
 for (let person of people) {
+  if (person === 'John Doe') {
+    console.log('Encountered John Doe. Stopping....');
+    break;
+  }
+
   if (person.startsWith('Jenny')) {
     continue;
   }
 
-  if (person === 'John Doe') {
-    break;
+  console.log(person);
+  if (person.startsWith('S')) {
+    console.log('Hey, your name starts with "s"!');
   }
-  console.log('person: ', person);
-  // if ( person.includes( ' S' ) ) {
-  // if ( person.split( ' ' )[1].startsWith( 'S' ) ) {
-  if (/ S/.test(person)) {
-    console.log('Hey, your last name start with "s"!');
+
+  let names = person.split(' ');
+  if (names[1].startsWith('S')) {
+    console.log('Hey, your LAST name starts with "s"!');
+  }
+
+  //   let re = new RegExp('[A-z]+ S');
+  let re = /[A-z]+ S/;
+  if (re.test(person)) {
+    console.log('A regular expression says that your last name starts with "S"!');
+  }
+
+  if (person.includes(' S')) {
+    console.log('Your name includes an "S" which might start your last name.');
   }
 }
