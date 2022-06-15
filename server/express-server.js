07e4e6709dea212e33e546a21c702907933fdbf2
John Paxton <johnpaxton@gmail.com>
2022-06-15 01:23:18 -0400

    Fix issue with ordering of routes

--- server/express-server.js
+++ server/express-server.js
@@ -1,22 +1,25 @@
 let express = require('express');
 let serveIndex = require('serve-index');
+let cors = require('cors');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let helpersRouter = require('./routes/helpers');
+let projectRoot = path.join(__dirname, '../');
 
 let app = express();
-let projectRoot = path.join(__dirname, '../');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', express.static(projectRoot), serveIndex(projectRoot, { icons: true }));
-
 app.use('/', indexRouter);
 app.use('/helpers', helpersRouter);
+
+// Make sure this is the last item
+app.use('/', express.static(projectRoot), serveIndex(projectRoot, { icons: true }));
 
 module.exports = app;
