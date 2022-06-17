2b47589f297a0d3a32c581c99cbf4bf9ba1769cb
John Paxton <johnpaxton@gmail.com>
2022-06-17 15:45:53 +0000

    More on iterators vs loops

--- demos/js/arrays/array-iterator-comparison.js
+++ demos/js/arrays/array-iterator-comparison.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 let names = [
   'Aish',
   'Allan',
@@ -19,3 +20,49 @@ let names = [
 for (let name of names) {
   console.log(name);
 }
+
+// With an iterator function
+names.forEach((name) => console.log(name));
+
+// Find names that match criteria
+// Names that start with 'a'
+
+// As a loop
+let matchingNames = [];
+for (let bar of names) {
+  if (bar.startsWith('A')) {
+    matchingNames.push(bar);
+  }
+}
+
+console.log('Names that start with A:', matchingNames);
+
+// Iterator function (filter)
+
+// let matchingNamesIterator = names.filter((name) => name.startsWith('A'));
+let matchingNamesIterator = names.filter((foo) => foo.startsWith('A'));
+let nonMatchingNamesIterator = names.filter((x) => !x.startsWith('A'));
+console.log('Names that start with A:', matchingNamesIterator);
+
+// Returns only the first value where the function returns true
+// If the function never returns true, find() returns null
+let firstMatchingName = names.find((name) => name.startsWith('A'));
+
+let numbers = [1, 2, 3, 4, 5];
+
+// Do some math to the numbers!
+// Square each number
+
+let squaredNumbers = [];
+for (let n of numbers) {
+  squaredNumbers.push(n * n);
+}
+
+let mappedNumbers = numbers.map((q) => q * q);
+
+// Turn names into list items
+let listItemNames = names.map((name) => {
+  let item = document.createElement('li');
+  item.textContent = name;
+  return item;
+});
