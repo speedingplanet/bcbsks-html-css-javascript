d163cf6ad63aa5e88eaf4e229ebbe29a80047f68
John Paxton <johnpaxton@gmail.com>
2022-06-17 15:22:24 +0000

    Array iterators

---
+++ demos/js/arrays/array-iterator-comparison.js
@@ -0,0 +1,21 @@
+let names = [
+  'Aish',
+  'Allan',
+  'Amir',
+  'Angela',
+  'Doug',
+  'Fatena',
+  'Jacob',
+  'John',
+  'John',
+  'Jonah',
+  'Srinivasa',
+  'Stephanie',
+];
+
+// Print out everyone's name:
+
+// As a loop
+for (let name of names) {
+  console.log(name);
+}
